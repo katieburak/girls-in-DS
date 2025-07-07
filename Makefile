9240c906355c2439368b9f30511b1868108591e1
Katie Burak <katieburak@gmail.com>
2025-07-07 14:41:54 -0700

    added worksheet 1 solution

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ book:
 	jupyter-book build .
 	cp -r _build/html/* docs
 	touch docs/.nojekyll
+
--- build.sh
+++
@@ -1,4 +0,0 @@
-jb clean ./
-jb build ./
-python script/fix_html.py /Users/tbeuzen/Work/574/students_574/_build/html/lectures/
-cp lectures/keplergl/* _build/html/_images
